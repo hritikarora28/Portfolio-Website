afaeb6485a416f0a47be85a71a14f944fcf6c214
Hritik Arora <hritikarora875@gmail.com>
2021-05-14 22:53:03 +0530

    created_static_path

--- src/app.js
+++ src/app.js
@@ -1,21 +1,26 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 const port = process.env.port || 8000;
+const staticPath = path.join(__dirname, '../public');
 
-app.get('',(req,res)=>{
-    res.send("we are in home page")
-})
-app.get('/about',(req,res)=>{
-    res.send("we are in about page")
-})
-app.get('/contact',(req,res)=>{
-    res.send("We are in about contact page")
-})
-app.get('*',(req,res)=>{
-    res.send("We are 404 error page")
-})
+app.use(express.static(staticPath));
 
-app.listen(port, ()=>{
-    console.log(`Your request is send to port-number ${port}`)
-})
+//routing
+app.get('', (req, res) => {
+	res.send('we are in home page');
+});
+app.get('/about', (req, res) => {
+	res.send('we are in about page');
+});
+app.get('/contact', (req, res) => {
+	res.send('We are in  contact page');
+});
+app.get('*', (req, res) => {
+	res.send('We are 404 error page');
+});
+
+app.listen(port, () => {
+	console.log(`Your request is send to port-number ${port}`);
+});
