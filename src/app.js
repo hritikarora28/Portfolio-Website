ca1ed34b5c17ebf1388d20b81a3b9f7366bbe1a1
Hritik Arora <hritikarora875@gmail.com>
2021-05-20 22:41:47 +0530

    hbs_added,404page_added

--- src/app.js
+++ src/app.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs')
 
 const app = express();
 const port = process.env.port || 8000;
 const staticPath = path.join(__dirname, '../public');
+const templatePath = path.join(__dirname,'../templates/views');
+const partialpath = path.join(__dirname,'../templates/partials');
 
 app.use(express.static(staticPath));
+app.set('view engine','hbs');
+app.set('views',templatePath);
+hbs.registerPartials(partialpath);
 
 //routing
 app.get('', (req, res) => {
-	res.send('we are in home page');
+	res.render('index');
 });
 app.get('/about', (req, res) => {
-	res.send('we are in about page');
+	res.render('about');
 });
 app.get('/contact', (req, res) => {
-	res.send('We are in  contact page');
+	res.render('contact');
 });
 app.get('*', (req, res) => {
-	res.send('We are 404 error page');
+	res.render('404', {
+		errormsg: 'Are you lost baby girl',
+	});
 });
 
 app.listen(port, () => {
